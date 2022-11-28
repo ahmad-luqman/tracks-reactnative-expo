ffeb16975c6349dc82e4a87eb94aece1b4e5b2ad
Ahmad Luqman <ahmad-luqman@users.noreply.github.com>
2022-11-28 21:52:54 +0500

    extract logic to useLocation hook

---
+++ src/hooks/useLocation.js
@@ -0,0 +1,37 @@
+import { useState, useEffect } from "react";
+import {
+  Accuracy,
+  requestForegroundPermissionsAsync,
+  watchPositionAsync,
+} from "expo-location";
+
+export default (callback) => {
+  const [err, setErr] = useState(null);
+
+  const startWatching = async () => {
+    try {
+      const { granted } = await requestForegroundPermissionsAsync();
+      if (!granted) {
+        throw new Error("Location permission not granted");
+      }
+      console.log("startWatching");
+      await watchPositionAsync(
+        {
+          accuracy: Accuracy.BestForNavigation,
+          timeInterval: 1000,
+          distanceInterval: 10,
+        },
+        callback
+      );
+      console.log("startWatching!!!");
+    } catch (e) {
+      setErr(e);
+    }
+  };
+
+  useEffect(() => {
+    startWatching();
+  }, []);
+  
+  return [err];
+};
--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,41 +1,18 @@
 import '../_mockLocation';
-import React, { useEffect, useState, useContext } from "react";
+import React, { useContext } from "react";
 import { Text, StyleSheet } from "react-native";
 import { SafeAreaView } from "react-navigation";
-import { requestForegroundPermissionsAsync, watchPositionAsync, Accuracy } from "expo-location";
 import Map from "../components/Map";
 import { Context as LocationContext } from "../context/LocationContext";
+import useLocation from '../hooks/useLocation';
 
 const TrackCreateScreen = () => {
   const { addLocation } = useContext(LocationContext);
-  const [err, setErr] = useState(null);
-  
-  const startWatching = async () => {
-    try {
-      const { granted } = await requestForegroundPermissionsAsync();
-      if (!granted) {
-        throw new Error("Location permission not granted");
-      }
-
-      await watchPositionAsync({
-        accuracy: Accuracy.BestForNavigation,
-        timeInterval: 1000,
-        distanceInterval: 10,
-      }, (location) => {
-        addLocation(location);
-      });
-    } catch (e) {
-      setErr(e);
-    }
-  };
-
-  useEffect(() => {
-    startWatching();
-  }, []);
+  const [err] = useLocation(addLocation);
 
   return (
     <SafeAreaView style={styles.container} forceInset={{ top: "always" }}>
-      <Text style={{ fontSize: 48 }}>TrackCreateScreen</Text>
+      <Text style={{ fontSize: 48 }}>Create a Track</Text>
       <Map />
       {err ? <Text>Please enable location services</Text> : null}
     </SafeAreaView>
